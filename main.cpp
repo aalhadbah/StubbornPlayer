fa50799f67a763286049a8f2540204bbdb289205
aalhadbah <aalhadbah@mail.csuchico.edu>
2018-03-01 22:04:18 -0800

    initial work

--- main.cpp
+++ main.cpp
@@ -3,9 +3,48 @@
 
 using namespace std;
 
+
+
+
 int main()
 {
-  SecretDoor game;
-    
-  return 0;
+        SecretDoor game;
+
+        //how may times user wants to play
+        int numPlays;
+
+        //current time
+        int currentTimes = 0;
+
+        //number of wins
+        int numWins = 0;
+
+        cout << "How many times you want to play game: ";
+        cin >> numPlays;
+
+        //loop and play game
+        for(int index=0; index<numPlays; index++)
+        {
+                
+                game.newGame();
+
+                
+                game.guessDoorC();
+
+                
+                game.guessDoorC();
+
+            }    //check win
+                if (game.isWinner() == true)
+                {
+                        numWins++;
+
+                }
+
+        //display how many times user won
+        cout << "The number of times you won: " << numWins << endl;
+
+
+
+        return 0;
 }
